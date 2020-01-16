7cd2aa624c75589ad6527bb248668c0799ff63f7
Lee Lazarecky <leelazarecky@gmail.com>
2020-01-15 23:46:13 -0500

    Reduces padding on mobile.

--- src/pages/blog/Index.vue
+++ src/pages/blog/Index.vue
@@ -68,86 +68,89 @@
 
             box-sizing: border-box;
             padding: 100px 64px 64px 64px;
-        }
-    }
-
-    .BlogEntryPreview {
-
-        width: 100%;
-
-        .link-wrapper {
-            width: 100%;
-            height: 320px;
-
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-bottom: 32px;
-            padding: 24px;
-            color: $primary-font-color;
-            text-decoration: none;
-            box-sizing: border-box;
 
             @media only screen and (max-width: $mobile-width) {
-                flex-direction: column;
-                margin-bottom: 64px;
-                padding-left: 0;
-                padding-right: 0;
-            }
-
-            &:hover {
-                background-color: rgba(black, .05);
-            }
-
-            img {
-                height: 100%;
-                width: 480px;
-                object-fit: cover;
-
-                @media only screen and (max-width: $mobile-width) {
-                    width: 100%;
-                }
-            }
-
-            .no-img {
-                height: 100%;
-                width: 480px;
-
-                @media only screen and (max-width: $mobile-width) {
-                    width: 100%;
-                }
-            }
-
-            .preview-text-container {
-                margin-left: 36px;
-                height: 100%;
-                width: 350px;
-
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-                justify-content: flex-start;
-
-                @media only screen and (max-width: $mobile-width) {
-                    width: 100%;
-                    justify-content: center;
-                    margin-left: 0;
-                    margin-top: 12px;
-                }
-
-                .preview-heading {
-                    font-size: 2rem;
-                    font-weight: bold;
-                    line-height: 2rem;
-                }
-
-                .preview-sub-heading {
-                    font-size: 1.2rem;
-                    opacity: .54;
-                }
+                padding: 64px 32px 32px 32px;
             }
         }
 
+        .BlogEntryPreview {
+
+            width: 100%;
+
+            .link-wrapper {
+                width: 100%;
+                height: 320px;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                margin-bottom: 32px;
+                padding: 24px;
+                color: $primary-font-color;
+                text-decoration: none;
+                box-sizing: border-box;
+
+                @media only screen and (max-width: $mobile-width) {
+                    flex-direction: column;
+                    margin-bottom: 64px;
+                    padding-left: 0;
+                    padding-right: 0;
+                }
+
+                &:hover {
+                    background-color: rgba(black, .05);
+                }
+
+                img {
+                    height: 100%;
+                    width: 480px;
+                    object-fit: cover;
+
+                    @media only screen and (max-width: $mobile-width) {
+                        width: 100%;
+                    }
+                }
+
+                .no-img {
+                    height: 100%;
+                    width: 480px;
+
+                    @media only screen and (max-width: $mobile-width) {
+                        width: 100%;
+                    }
+                }
+
+                .preview-text-container {
+                    margin-left: 36px;
+                    height: 100%;
+                    width: 350px;
+
+                    display: flex;
+                    flex-direction: column;
+                    align-items: flex-start;
+                    justify-content: flex-start;
+
+                    @media only screen and (max-width: $mobile-width) {
+                        width: 100%;
+                        justify-content: center;
+                        margin-left: 0;
+                        margin-top: 12px;
+                    }
+
+                    .preview-heading {
+                        font-size: 2rem;
+                        font-weight: bold;
+                        line-height: 2rem;
+                    }
+
+                    .preview-sub-heading {
+                        font-size: 1.2rem;
+                        opacity: .54;
+                    }
+                }
+            }
+        }
 
     }
 </style>
